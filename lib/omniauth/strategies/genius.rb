bf11d94df80cce6d1be25edaed6f69b88cf03430
John Crepezzi <john@genius.com>
2015-05-03 09:19:50 -0400

    Removed ENV option in favor of default OmniAuth config style

--- lib/omniauth/strategies/genius.rb
+++ lib/omniauth/strategies/genius.rb
@@ -7,7 +7,7 @@ module OmniAuth
       option :name, 'genius'
 
       option :client_options, {
-        site: ENV.fetch('GENIUS_SITE', 'https://api.genius.com')
+        site: 'https://api.genius.com'
       }
 
       uid do
